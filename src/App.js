5cafef0f9bf3e913ff8af990f54db868d718fddc
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-11-03 17:19:01 +0900

    3-2 属性を利用する

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super();
+    this.title = props.title;
+    this.message = props.message;
+  }
+
   render(){
     return <div>
-      <h1>React</h1>
-      <p>This is sample component.</p>
-      <p>これはサンプルのコンポーネントです。</p>
-    </div>;
-  };
+      <h1>{this.title}</h1>
+      <p>{this.message}</p>
+    </div>
+  }
 }
 
 // 外部から利用できるようにしている
