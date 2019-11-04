cc84fd5eaf70d8b303f0ce5f6fe40eee8147a1ea
muroga_romu <3_3-benzouspirit@ezweb.ne.jp>
2019-11-04 21:51:38 +0900

    3-3 ステートの更新

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      msg: "Hello Component."
+      msg: "Hello",
     }
+    let timer = setInterval(() => {
+      this.setState((state) => ({msg: state.msg + "!"}));
+    }, 10000);
   }
 
   render(){
